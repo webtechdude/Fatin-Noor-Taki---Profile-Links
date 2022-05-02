820756309ffec0d24543811d793d48ece64909bc
WebTechDUDE <takenolove13579@gmail.com>
2022-05-03 00:48:54 +0600

    mnaythings

--- js/index.js
+++ js/index.js
@@ -1,27 +1,21 @@
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 20) + "px; left: " + (e.pageX - 20) + "px;")
+  cursor.setAttribute("style", "top: " + (e.pageY - 20) + "px; left: " + (e.pageX - 20) + "px;")
 })
 
 document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+  cursor.classList.add("expand");
 
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500)
 })
-
-
-
-
-
-
 let resizeTimer;
 window.addEventListener("resize", () => {
   document.body.classList.add("resize-animation-stopper");
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(() => {
-    document.   body.classList.remove("resize-animation-stopper");
+    document.body.classList.remove("resize-animation-stopper");
   }, 400);
 });
